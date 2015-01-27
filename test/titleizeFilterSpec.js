ccb1b65e5a66c7c94b649e3837eb12553f7e62d1
Kyle Paul <kpaul86+github@gmail.com>
2015-01-27 22:54:26 +0000

    Step 21: Testing a filter

---
+++ test/titleizeFilterSpec.js
@@ -0,0 +1,16 @@
+describe('Titleize filter', function() {
+  var filter;
+  
+  beforeEach(function() {
+    angular.mock.module('myApp.filters');
+    angular.mock.inject(function($filter) {
+      filter = $filter('titleize');
+    });
+  });
+  
+  it('converts string to titleized string', function() {
+    expect(filter("MY UPPERCASE STRING")).toEqual("My Uppercase String");
+    expect(filter("my lowercase string")).toEqual("My Lowercase String");
+    expect(filter("My MiXeD cAsE sTrInG")).toEqual("My Mixed Case String");
+  });
+});
