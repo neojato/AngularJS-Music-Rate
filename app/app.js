d6a7254d808f2de32bc8087eeb40f8cba03921b6
Kyle Paul <kpaul86+github@gmail.com>
2015-01-27 07:05:20 +0000

    Step 6: FiLTEring

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 angular.module("myApp", [
+  'myApp.filters',
   'myApp.controllers'
 ]);
 
---
+++ app/filters.js
@@ -0,0 +1,6 @@
+angular.module("myApp.filters", [])
+	.filter("titleize", function() {
+  		return function(input) {
+        return _.titleize(input);
+      };
+	});
