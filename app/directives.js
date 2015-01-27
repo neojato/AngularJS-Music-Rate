93ade6f4bee1451eae3c37f230106778557f2f65
Kyle Paul <kpaul86+github@gmail.com>
2015-01-27 20:33:42 +0000

    Step 14: First directive, second version

--- app/directives.js
+++ app/directives.js
@@ -1,6 +1,6 @@
 angular.module("myApp.directives", [])
   .directive("rating", function() {
-    var directive = { };
+    var directive = {};
     directive.restrict = 'AE';
 
     directive.scope = {
@@ -9,6 +9,32 @@ angular.module("myApp.directives", [])
     };
 
     directive.templateUrl = "app/templates/rating.html";
+
+    directive.link = function(scope, elements, attr) {
+      scope.updateStars = function() {
+        var idx = 0;
+        scope.stars = [ ];
+        for(idx = 0; idx < scope.max; idx += 1) {
+          scope.stars.push({
+            full: scope.score > idx
+          });
+        }
+      };
+      
+      scope.starClass = function(star, idx) {
+        var starClass = 'fa-star-o';
+        if(star.full) {
+          starClass = 'fa-star';
+        }
+        return starClass;
+      };
+      
+      scope.$watch('score', function(newValue, oldValue) {
+        if(newValue !== null && newValue !== undefined) {
+          scope.updateStars();
+        }
+      });
+    };
 
     return directive;
   });
